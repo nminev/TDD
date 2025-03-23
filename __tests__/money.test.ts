e771f5db358ed73f59710e4c2dd7cbcba2ec4632
nikolay <nikolay.minev@runa.io>
2025-03-23 20:01:47 +0200

    first commit

---
+++ __tests__/money.test.ts
@@ -0,0 +1,20 @@
+import { Dollar } from '../src/money'
+
+// 1. Quickly add a test.
+// 2. Run all tests and see the new one fail
+// 3. Make a little change
+// 4. Run all tests and see them all succeed
+// 5. Refactor to remove duplication
+
+// TODO:
+// [ ]  $5 + 10 CHF = $10 if rate is 2:1
+// [ ]  $5 * 2 = $10 - WIP
+// [ ]  Make amount private
+// [ ]  Dollar side effects ?
+// [ ]  Money rounding ?
+
+test('5 dollars times 2 is 10 dollars', () => {
+  const five = new Dollar(5)
+  five.times(2)
+  expect(five.amount).toBe(10)
+})
---
+++ src/money.ts
@@ -0,0 +1,12 @@
+// money.ts
+
+export class Dollar {
+    amount: number;
+
+    constructor(amount: number) {
+        this.amount = amount;
+    }
+    times(multiplier: number) {
+        this.amount = 10;
+    }
+}
