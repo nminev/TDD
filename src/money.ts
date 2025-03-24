7cf711dc80cea748591b7ae91ba84a789810d778
nikolay <nikolay.minev@runa.io>
2025-03-24 12:51:37 +0200

    Wallet solution

--- src/money.ts
+++ src/money.ts
@@ -17,4 +17,30 @@ export class Money {
         const sameType = this.currency === money.currency;
         return sameType && this.amount === money.amount;
     }
+
+    getCurrency(): string {
+        return this.currency;
+    }
+
+    plus(money: Money): Money {
+        return new Money(this.amount + money.amount, this.currency);
+    }
+}
+
+export class Wallet {
+    private currencies: Map<string, Money> = new Map();
+    constructor() { }
+
+    add(money: Money) {
+        const currency = money.getCurrency();
+        if (!this.currencies.has(currency)) {
+            this.currencies.set(currency, new Money(0, currency));
+        }
+        const currentBalance = this.currencies.get(currency)!.plus(money);
+        this.currencies.set(currency, currentBalance);
+    }
+
+    balance(currency: string): Money {
+        return this.currencies.get(currency)!;
+    }
 }
--- test/money.test.ts
+++ test/money.test.ts
@@ -1,4 +1,4 @@
-import { Money } from "../src/money.ts"
+import { Money, Wallet } from "../src/money.ts"
 import { assertEquals } from "jsr:@std/assert"
 
 // 1. Quickly add a test.
@@ -10,13 +10,28 @@ import { assertEquals } from "jsr:@std/assert"
 // TODO:
 // [ ]  $5 + 10 CHF = $10 if rate is 2:1
 // [ ]  $5 + $5 = $10 - WIP
+// [ ]  What I own in a specific curreny
+// [ ]  What is my overall balance in a specific currency
 
-Deno.test("Test Dollar addition", () => {
+Deno.test("Test Dollar balance", () => {
   const fiveDollars: Money = new Money(5, "USD")
-  const tenDollars: Money = new Money(10, "USD")
-  const sum: Money = fiveDollars.plus(fiveDollars)
-  assertEquals(sum.equals(tenDollars), true)
+  const wallet: Wallet = new Wallet()
+  wallet.add(fiveDollars)
+  wallet.add(fiveDollars)
+  console.log(wallet.balance("USD"))
+  assertEquals(wallet.balance("USD").equals(new Money(10, "USD")), true)
 })
+
+// Deno.test("Test Dollar addition", () => {
+//   const fiveDollars: Money = new Money(5, "USD")
+//   const tenFranc: Money = new Money(10, "CHF")
+//   const wallet: Wallet = new Wallet()
+//   wallet.add(fiveDollars)
+//   wallet.add(fiveDollars)
+//   wallet.add(tenFranc) // Given its 2:1, this should be 5 USD
+//   assertEquals(wallet.total("USD").equals(15), true)
+// })
+
 
 Deno.test("Test Dollar multiplication", () => {
   const fiveDollars = new Money(5, "USD")
