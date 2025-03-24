86f5c0f8bd53d12ed2247628c68eeeffdf5afea1
nikolay <nikolay.minev@runa.io>
2025-03-24 12:55:20 +0200

    clean slate, from begining

--- test/money.test.ts
+++ test/money.test.ts
@@ -1,4 +1,4 @@
-import { Money, Wallet } from "../src/money.ts"
+// import { } from "../src/money.ts"
 import { assertEquals } from "jsr:@std/assert"
 
 // 1. Quickly add a test.
@@ -9,46 +9,10 @@ import { assertEquals } from "jsr:@std/assert"
 
 // TODO:
 // [ ]  $5 + 10 CHF = $10 if rate is 2:1
-// [ ]  $5 + $5 = $10 - WIP
-// [ ]  What I own in a specific curreny
-// [ ]  What is my overall balance in a specific currency
+// [ ]  $5 *2 = $10 - WIP
 
-Deno.test("Test Dollar balance", () => {
-  const fiveDollars: Money = new Money(5, "USD")
-  const wallet: Wallet = new Wallet()
-  wallet.add(fiveDollars)
-  wallet.add(fiveDollars)
-  console.log(wallet.balance("USD"))
-  assertEquals(wallet.balance("USD").equals(new Money(10, "USD")), true)
+Deno.test("5 dollars times 2 is 10 dollars", () => {
+  const five = new Dollar(5, "USD");
+  const product = five.times(2)
+  assertEquals(product.amount, 10)
 })
-
-// Deno.test("Test Dollar addition", () => {
-//   const fiveDollars: Money = new Money(5, "USD")
-//   const tenFranc: Money = new Money(10, "CHF")
-//   const wallet: Wallet = new Wallet()
-//   wallet.add(fiveDollars)
-//   wallet.add(fiveDollars)
-//   wallet.add(tenFranc) // Given its 2:1, this should be 5 USD
-//   assertEquals(wallet.total("USD").equals(15), true)
-// })
-
-
-Deno.test("Test Dollar multiplication", () => {
-  const fiveDollars = new Money(5, "USD")
-  assertEquals(fiveDollars.equals(new Money(5, "USD")), true)
-  assertEquals(fiveDollars.times(2).equals(new Money(10, "USD")), true)
-  assertEquals(fiveDollars.times(3).equals(new Money(15, "USD")), true)
-  const fiveFrancs = new Money(5, "CHF")
-  assertEquals(fiveFrancs.equals(new Money(5, "CHF")), true)
-  assertEquals(fiveFrancs.times(2).equals(new Money(10, "CHF")), true)
-  assertEquals(fiveFrancs.times(3).equals(new Money(15, "CHF")), true)
-})
-
-Deno.test("Test equals", () => {
-  assertEquals(new Money(5, "USD").equals(new Money(5, "USD")), true)
-  assertEquals(new Money(5, "USD").equals(new Money(6, "USD")), false)
-  assertEquals(new Money(5, "CHF").equals(new Money(5, "CHF")), true)
-  assertEquals(new Money(5, "CHF").equals(new Money(6, "CHF")), false)
-  assertEquals(new Money(5, "USD").equals(new Money(5, "CHF")), false)
-})
-
